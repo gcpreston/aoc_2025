2d57d75f996db91b8192897c69e575138966a8c9
Graham Preston <graham.preston@gmail.com>
2025-12-01 10:11:29 +0100

    Lib function input_for_day

--- lib/aoc2025.ex
+++ lib/aoc2025.ex
@@ -3,16 +3,24 @@ defmodule Aoc2025 do
   Documentation for `Aoc2025`.
   """
 
+  @input_directory "input"
+
   @doc """
-  Hello world.
+  Read the entire input file for a day. Raises `File.Error`
+  if file does not exist.
 
   ## Examples
 
-      iex> Aoc2025.hello()
-      :world
+      iex> Aoc2025.input_for_day(1)
+      "..."
+
+      iex> Aoc2025.input_for_day(26)
+      ** (File.Error) could not read file "input/26.txt": no such file or directory
 
   """
-  def hello do
-    :world
+  @spec input_for_day(integer()) :: String.t()
+  def input_for_day(day) do
+    Path.join([@input_directory, String.pad_leading("#{day}.txt", 6, "0")])
+    |> File.read!()
   end
 end
