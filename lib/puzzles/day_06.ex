0de5be3dccfbe9f311b272348af31c326eb1014e
Graham Preston <graham.preston@gmail.com>
2025-12-07 00:37:14 +0100

    Day 6 part 2

--- lib/puzzles/day_06.ex
+++ lib/puzzles/day_06.ex
@@ -14,7 +14,22 @@ defmodule Aoc2025.Day06 do
   end
 
   def part_2(input) do
+    data = parse_input_part_2(input)
 
+    nums =
+      Enum.slice(data, 0..(length(data) - 2))
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&list_to_int/1)
+      |> list_split(nil)
+
+    ops = Enum.at(data, length(data) - 1) |> Enum.join() |> String.split()
+
+    [ops, nums]
+    |> Enum.zip()
+    |> Enum.reduce(0, fn {o, n}, total ->
+      total + do_operation(o, n)
+    end)
   end
 
   def parse_input(input) do
@@ -35,6 +50,44 @@ defmodule Aoc2025.Day06 do
   end
 
   def parse_input_part_2(input) do
-    
+    unfilled_data =
+      input
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
+
+    max_len = unfilled_data |> Enum.map(&length/1) |> Enum.max()
+
+    Enum.map(unfilled_data, fn line -> pad(line, max_len, " ") end)
   end
+
+  def pad(list, len, e) do
+    diff = len - length(list)
+    if diff > 0 do
+      list ++ Enum.map(1..diff, fn _ -> e end)
+    else
+      list
+    end
+  end
+
+  def list_to_int(list) do
+    parse_result =
+      list
+      |> Enum.join()
+      |> String.trim()
+      |> Integer.parse()
+
+    case parse_result do
+      :error -> nil
+      {i, _} -> i
+    end
+  end
+
+  def list_split(list, by), do: list_split(list, by, [[]])
+
+  def list_split([], _by, acc), do: Enum.reverse(Enum.map(acc, &Enum.reverse/1))
+  def list_split([head | rest], by, acc) when head == by, do: list_split(rest, by, [[] | acc])
+  def list_split([head | rest], by, [acc_head | acc_rest]), do: list_split(rest, by, [[head | acc_head] | acc_rest])
+
+  def do_operation("+", operands), do: Enum.sum(operands)
+  def do_operation("*", operands), do: Enum.product(operands)
 end
