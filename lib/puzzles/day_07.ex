86e403179349ef6ad1bdea73c8ec9732f112e995
Graham Preston <graham.preston@gmail.com>
2025-12-07 18:28:36 +0100

    Day 7 part 2

--- lib/puzzles/day_07.ex
+++ lib/puzzles/day_07.ex
@@ -8,7 +8,8 @@ defmodule Aoc2025.Day07 do
   end
 
   def part_2(input) do
-
+    {start, splitters} = parse_input(input)
+    count_paths(splitters, %{start => 1})
   end
 
   @spec parse_input(String.t()) :: data()
@@ -35,14 +36,10 @@ defmodule Aoc2025.Day07 do
   def count_splits([head_splitters | rest], beams, count) do
     {beams_to_split, beams_to_not_split} = MapSet.split_with(beams, fn e -> e in head_splitters end)
 
-    if MapSet.size(beams_to_split) == 0 do
-      count_splits(rest, beams, count)
-    else
-      new_beams = compute_new_beams(beams_to_split, beams_to_not_split)
-      split_count = MapSet.size(beams_to_split)
-      # dbg({head_splitters, beams, new_beams, split_count})
-      count_splits(rest, new_beams, count + split_count)
-    end
+    new_beams = compute_new_beams(beams_to_split, beams_to_not_split)
+    split_count = MapSet.size(beams_to_split)
+    # dbg({head_splitters, beams, new_beams, split_count})
+    count_splits(rest, new_beams, count + split_count)
   end
 
   def compute_new_beams(to_split, dont_split) do
@@ -50,5 +47,37 @@ defmodule Aoc2025.Day07 do
       MapSet.new(Enum.flat_map(to_split, fn b -> [b - 1, b + 1] end)),
       dont_split
     )
+  end
+
+  def count_paths([], counts) do
+    counts
+    |> Enum.map(fn {_i, c} -> c end)
+    |> Enum.sum()
+  end
+
+  def count_paths([head_splitters | rest], counts) do
+    new_counts = split_paths(head_splitters, counts)
+    count_paths(rest, new_counts)
+  end
+
+  def split_paths(head_splitters, counts) do
+    # [{6, 1}, {8, 1}, {9, 4}], [6, 8]
+    # --> [{5, 1}, {7, 2}, {9, 5}]
+
+    beams = MapSet.new(Map.keys(counts))
+    {beams_to_split, beams_to_not_split} = MapSet.split_with(beams, fn e -> e in head_splitters end)
+
+    split_counts =
+      beams_to_split
+      |> Enum.flat_map(fn b -> [{b - 1, Map.get(counts, b, 1)}, {b + 1, Map.get(counts, b, 1)}] end) # split
+      # |> dbg()
+      |> Enum.reduce(%{}, fn {b, c}, acc ->
+        # dbg({b, c, acc})
+        Map.update(acc, b, c, fn old_count -> old_count + c end)
+      end) # combine counts
+
+    counts
+    |> Map.filter(fn {k, _v} -> k in beams_to_not_split end)
+    |> Map.merge(split_counts, fn _k, v1, v2 -> v1 + v2 end)
   end
 end
